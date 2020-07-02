649784c40c77cc0a3fc59ef1bb2d7d13d2071cd9
詹志强 <zqzhan@kaikeba.com>
2020-07-02 19:50:28 +0800

    feat: 案例demo

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,76 +1,84 @@
 import React from 'react';
 import Head from 'next/head';
-import { connect } from 'react-redux';
-import Nav from '../../components/Nav';
 import styles from './styles.styl';
 
-const Home = props => (
-  <div>
-    <Head>
-      <title>Home</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+const Home = props => {
+  return (
+    <div>
+      <Head>
+        <title>Home</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-    <div className="hero">
-      <h1 className="title">Welcome to blog page! {props.demoName}</h1>
+      <div className="hero">
+        <h1 className="title">Welcome to blog page! {props.demoName}</h1>
 
-      <div className={styles.stark}>This is blog page</div>
+        <div className={styles.stark}>This is blog page</div>
 
-      <p className="description">
-        To get started, edit <code>pages/index.js</code> and save to reload.
-      </p>
+        <p className="description">
+          To get started, edit <code>pages/index.js</code> and save to reload.
+        </p>
+      </div>
+
+      <style jsx>{`
+        .hero {
+          width: 100%;
+          color: #333;
+        }
+        .title {
+          margin: 0;
+          width: 100%;
+          padding-top: 80px;
+          line-height: 1.15;
+          font-size: 48px;
+        }
+        .title,
+        .description {
+          text-align: center;
+        }
+        .row {
+          max-width: 880px;
+          margin: 80px auto 40px;
+          display: flex;
+          flex-direction: row;
+          justify-content: space-around;
+        }
+        .card {
+          padding: 18px 18px 24px;
+          width: 220px;
+          text-align: left;
+          text-decoration: none;
+          color: #434343;
+          border: 1px solid #9b9b9b;
+        }
+        .card:hover {
+          border-color: #067df7;
+        }
+        .card h3 {
+          margin: 0;
+          color: #067df7;
+          font-size: 18px;
+        }
+        .card p {
+          margin: 0;
+          padding: 12px 0 0;
+          font-size: 13px;
+          color: #333;
+        }
+      `}</style>
     </div>
+  );
+};
 
-    <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title,
-      .description {
-        text-align: center;
-      }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
-    `}</style>
-  </div>
-);
+// 函数hook组件使用方式，在组件上挂载此方法
+Home.getInitialProps = async ({ store }) => {
+  const getHomeFn = store.dispatch.home; // 获取redux中home挂载的方法
+  const getHomeState = store.getState().home; // 获取redux中home的state值
 
-export default connect(
-  ({ home }) => ({ ...home }),
-  ({ home }) => ({ ...home })
-)(Home);
+  return {
+    demoName: getHomeState.demoName,
+    ...getHomeFn
+  };
+};
+
+export default Home;
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,24 @@
 import axios from 'axios';
 import config from './config';
 
+const codeMessage = {
+  200: '服务器成功返回请求的数据。',
+  201: '新建或修改数据成功。',
+  202: '一个请求已经进入后台排队（异步任务）。',
+  204: '删除数据成功。',
+  400: '400接口请求失败，请重试！如有疑问，联系管理员。。',
+  401: '用户没有权限（令牌、用户名、密码错误）。',
+  403: '抱歉！你暂无权限操作此功能。',
+  404: '404接口请求失败，请重试！如有疑问，联系管理员。。',
+  406: '请求的格式不可得。',
+  410: '请求的资源被永久删除，且不会再得到的。',
+  422: '当创建一个对象时，发生一个验证错误。',
+  500: '服务器发生错误，请检查服务器。',
+  502: '网关错误。',
+  503: '服务不可用，服务器暂时过载或维护。',
+  504: '网关超时。'
+};
+
 axios.defaults.withCredentials = true;
 axios.defaults.timeout = 50000;
 axios.defaults.headers.common['Content-Type'] = 'application/json';
@@ -17,14 +35,9 @@ axios.interceptors.response.use(
       return;
     }
     const res = err.response;
-    if ([500, 502, 503].indexOf(res.status) > -1) {
-      console.error('500服务端错误，请稍后重试！');
-    } else if (res.status === 401) {
-      console.error('需要登陆授权');
-    } else if (res.status === 403) {
-      console.error('抱歉！你暂无权限操作此功能');
-    } else if ([400, 404].indexOf(res.status) > -1) {
-      console.error('400/404 接口请求失败，请重试！如有疑问，联系管理员。');
+    if (codeMessage[res?.status]) {
+      // eslint-disable-next-line
+      console.error(`${codeMessage[res?.status]}`);
     }
   }
 );
